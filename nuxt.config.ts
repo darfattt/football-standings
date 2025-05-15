595078042f3700ee0db843b36a4000d52a4b0fa0
darmawanfatriananda <darmawan.fatriananda@mandrill.com.my>
2025-05-15 09:04:26 +0700

    ke

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
   debug: true,
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.SUPABASE_URL || 'https://oolkuvhrpsvpfkvutaav.supabase.co',
-      supabaseKey: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbGt1dmhycHN2cGZrdnV0YWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzMTA4MDcsImV4cCI6MjA1ODg4NjgwN30.81ZkaLahHymPkL35jHfBnbpTGtdx339UwsJAmd8eIDg'
+      supabaseUrl: process.env.SUPABASE_URL || '',
+      supabaseKey: process.env.SUPABASE_KEY || ''
     }
   }
 })
